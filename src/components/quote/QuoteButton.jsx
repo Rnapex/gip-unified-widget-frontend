633a42ebd70e67ca3b7f26da99654ffd66353c02
Rnapex <raman@rnapex.ca>
2026-05-16 17:28:49 -0300

    Update QuoteButton.jsx

--- src/components/quote/QuoteButton.jsx
+++ src/components/quote/QuoteButton.jsx
@@ -1,22 +1,36 @@
+// src/components/quote/QuoteButton.jsx
+
 function QuoteButton({
+
   onClick,
+
   loading,
+
   disabled = false,
+
 }) {
 
   return (
+
     <button
+
       className="
         quote-button
       "
+
       onClick={onClick}
+
       disabled={
         loading || disabled
       }
     >
-      {loading
-        ? "Calculating..."
-        : "Get Quote"}
+
+      {
+        loading
+          ? "Calculating..."
+          : "Get Quote"
+      }
+
     </button>
   );
 }
