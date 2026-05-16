e3fc01cecbb11d51d1cb87fc7f168688dc4fe320
Rnapex <raman@rnapex.ca>
2026-05-16 15:17:11 -0300

    Update quoteApi.js

--- src/services/quoteApi.js
+++ src/services/quoteApi.js
@@ -3,16 +3,35 @@ import axios from "axios";
 import { API_BASE }
   from "../config/api";
 
+import {
+  getCaptchaToken,
+} from "./captchaService";
+
 export async function fetchQuote(
   payload
 ) {
 
   try {
 
+    const captchaToken =
+      await getCaptchaToken();
+
+    console.log(
+      "CAPTCHA:",
+      captchaToken
+    );
+
     const response =
       await axios.post(
         `${API_BASE}/api/quote/calculate`,
-        payload
+        payload,
+        {
+          headers: {
+
+            "x-captcha-token":
+              captchaToken,
+          },
+        }
       );
 
     console.log(
