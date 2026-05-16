8f93c07e8e1c974ccd5312cad34f2406f54133ec
Rnapex <raman@rnapex.ca>
2026-05-16 16:00:12 -0300

    Update captchaService.js

--- src/services/captchaService.js
+++ src/services/captchaService.js
@@ -1,27 +1,41 @@
-export async function getCaptchaToken() {
+export async function getCaptchaToken(
+  action = "calculate_ondemand_price"
+) {
 
   try {
 
     if (
       !window.grecaptcha
     ) {
+
       return null;
     }
 
+    const timestamp =
+      new Date().getTime();
+
+    const captchaAction =
+      `custom_customer_${action}_${timestamp}`;
+
     const token =
-      await window.grecaptcha
-        .enterprise.execute(
+      await window
+        .grecaptcha
+        .enterprise
+        .execute(
 
           import.meta.env
             .VITE_RECAPTCHA_SITE_KEY,
 
           {
             action:
-              "quote_request",
+              captchaAction,
           }
         );
 
-    return token;
+    return {
+      token,
+      timestamp,
+    };
 
   } catch (error) {
 
