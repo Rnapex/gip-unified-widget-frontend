30b1e9f7d79dc0b4aeab77a97c868d925957d553
Rnapex <raman@rnapex.ca>
2026-05-16 02:06:39 -0300

    Update serviceApi.js

--- src/services/serviceApi.js
+++ src/services/serviceApi.js
@@ -1,23 +1,14 @@
 import axios from "axios";
 
-const API_BASE =
-  "http://localhost:5000";
+import { API_BASE }
+  from "../config/api";
 
 export async function fetchServices() {
-  try {
-    const response =
-      await axios.get(
-        `${API_BASE}/api/widget/services`
-      );
 
-    return response.data || [];
-
-  } catch (error) {
-    console.error(
-      "Failed to fetch services",
-      error
+  const response =
+    await axios.get(
+      `${API_BASE}/api/widget/services`
     );
 
-    return [];
-  }
+  return response.data;
 }
