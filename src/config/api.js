82394e6e15c07132c408ef610dfa1b6fbd4ef6a2
Rnapex <raman@rnapex.ca>
2026-05-16 02:20:56 -0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,17 @@
 import axios from "axios";
 
 const API_BASE =
- 
+  import.meta.env
+    .VITE_API_BASE_URL;
+
+console.log(
+  "API BASE:",
+  API_BASE
+);
+
+const apiClient =
+  axios.create({
+    baseURL: API_BASE,
+  });
+
+export default apiClient;
